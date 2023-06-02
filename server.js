61483a00656c6f102b3eb1619b955d9422b1f382
--replace-all <imaryan08@gmail.com>
2023-06-03 01:53:01 +0530

    changes done

---
+++ server.js
@@ -0,0 +1,9 @@
+const app = require("./src/app");
+const dbConnection = require("./src/configs/db");
+
+const port = process.env.PORT || 3030;
+
+app.listen(port, async (req, res) => {
+  await dbConnection();
+  console.log(`server running on port ${port}`);
+});
--- src/server.js
+++
@@ -1,7 +0,0 @@
-const app = require("./app");
-const dbConnection = require("./configs/db.js");
-
-app.listen(8080, async (req, res) => {
-  await dbConnection();
-  console.log(`server running on port 8080`);
-});
